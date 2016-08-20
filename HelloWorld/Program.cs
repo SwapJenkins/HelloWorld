d96049ecf5c1b0ce5e34126f3e8cdf9cf771a485
SwapJenkins <swap.aher@gmail.com>
2016-08-20 13:38:28 -0400

    Hello world added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( CheckHelloWorld());
+        }
+
+        public static string CheckHelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ HelloWorldTests/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CheckHelloWorldTest()
+        {
+            Assert.AreEqual("Hello world", Program.CheckHelloWorld());
+
+        }
+    }
+}
